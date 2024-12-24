c6887b4dac11f5a41edcc3a4ff1c3376a9c523b9
Roham Jabbari <jabbari.roham@yahoo.com>
2024-12-24 15:16:25 +0100

    env activate

--- pair.sh
+++ pair.sh
@@ -2,7 +2,7 @@
 
 # Activate the conda environment
 source $HOME/.bashrc
-conda activate copa
+source /content/env/bin/activate
 
 # Run the Python script directly with the specified parameters
 env HYDRA_FULL_ERROR=1 python -m trident.run experiment=bpm_pair_clf +trainer.log_every_n_steps=10 run.seed=${1}
